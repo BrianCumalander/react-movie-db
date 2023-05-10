add862e1c69b84d3f59d86c49eafe2dab0ee4865
BrianCumalander <koomo24@gmail.com>
2023-05-10 08:41:51 -0600

    updated api key

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//create the element
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>,//then display it
   document.getElementById('root')
 );
