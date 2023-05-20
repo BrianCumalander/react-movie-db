6a1cf8d2a529bde11eb4b15b014e91d120821192
BrianCumalander <koomo24@gmail.com>
2023-05-20 13:02:36 -0600

    imported custoom hook useHomeFetch on Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,15 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 
 // Hook
+import { useHomeFetch } from '../hooks/useHomeFetch';
 
 // Image
 import NoImage from '../images/no_image.jpg'; //image can be named anything when importing like this. Here, we've named it NoImage. Also, be sure to include the file extension when importing like this.
 
 const Home = () => {
-    
-    console.log(state, "This information is listed 3 times because we defined 3 different states (lines 19-21), so it's rerendering 1 for each state.") //line 29 we could see the movies listed in console.log, but we need to test the entire code block, so at the bottom of the code block I'm running console.log(state) to check its state.
+    const { state, loading, error } = useHomeFetch();
+
+    console.log(state, "This information is listed several times because we defined a couple of states (lines 19-21), so it's rerendering 1 for each state.") 
     return <div>Home Page</div>
 }
 
