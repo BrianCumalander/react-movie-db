11b58c20ee6697fb74a25ed5a3373da84f7f4819
BrianCumalander <koomo24@gmail.com>
2023-05-20 14:59:44 -0600

    HeroImage styling

--- src/components/HeroImage/HeroImage.styles.js
+++ src/components/HeroImage/HeroImage.styles.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-    background: linear-gradient(
+    background: linear-gradient(    //**applies a linear gradient that makes it darker at the bottom of image so that the Text component will stand out, and you will be able to read it no matter what the image is.
         to bottom, 
         rgba(0,0,0,0) 41%, 
         rgba(0,0,0,0.65) 100%
     ),
-    url(${({ image}) => image}), var(--darkGrey);
+    url(${({ image }) => image}), var(--darkGrey);
     background-size: 100%, cover; //stops it from tiling
     background-position: center;
     height: 600px; //helps to display the entire background image
@@ -25,9 +25,39 @@ export const Wrapper = styled.div`
 `;
 
 export const Content = styled.div`
-
+    padding: 20px; //fixes padding around the entire heroImage. Also resolved the white bar that was between heroImage & header.
+    max-width: var(--maxWidth); //defined in config.js
+    margin: 0 auto;
 `;
 
 export const Text = styled.div`
+    z-index: 100; //just to place it on top of everything else
+    max-width: 700px;
+    position: absolute;
+    bottom: 40px; //moved the text to the bottom of heroImage, 40px from bottom of heroImage
+    margin-right: 20px;
+    min-height: 100px;
+    color: var(--white); //defined in config.js
+
+    h1 {
+        font-size: var(--fontSuperBig);
+
+        @media screen and (max-width: 720px) {
+            font-size: var(--fontBig);
+        }
+    }
+
+    p {
+        font-size: var(--fontMed);
+
+        @media screen and (max-width: 720px) {
+            font-size: var(--fontSmall);
+        }
+    }
+
+    @media screen and (max-width: 720px) { //setting for the screen itself
+        max-width: 100%;
+    }
+
 
 `;
