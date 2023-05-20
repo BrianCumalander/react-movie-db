599e0c93d04a7b0ffd8de29989e9888b7e62ad8c
BrianCumalander <koomo24@gmail.com>
2023-05-20 14:43:52 -0600

    styled HeroImage Wrapper component

--- src/components/HeroImage/HeroImage.styles.js
+++ src/components/HeroImage/HeroImage.styles.js
@@ -1,7 +1,33 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    background: linear-gradient(
+        to bottom, 
+        rgba(0,0,0,0) 41%, 
+        rgba(0,0,0,0.65) 100%
+    ),
+    url(${({ image}) => image}), var(--darkGrey);
+    background-size: 100%, cover; //stops it from tiling
+    background-position: center;
+    height: 600px; //helps to display the entire background image
+    position: relative; //setting to relative because we're going to set the text to center
+    animation: animateHeroImage 1s; //will fade from white to image in one second
 
-export const Content = styled.div``;
+    @keyframes animateHeroImage {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
 
-export const Text = styled.div``;
+`;
+
+export const Content = styled.div`
+
+`;
+
+export const Text = styled.div`
+
+`;
