1211541bf14073b56955fcbba3b9e4a559f577fb
BrianCumalander <koomo24@gmail.com>
2023-05-22 08:56:00 -0600

    styling the thumbnail & its image, & hover effect

--- src/components/Thumb/Thumb.styles.js
+++ src/components/Thumb/Thumb.styles.js
@@ -1,4 +1,26 @@
 import styled from 'styled-components';
 
 // Component
-export const Image = styled.img``; //styling the image
+// styling the thumbnail & its image
+export const Image = styled.img`
+    width: 100%;
+    max-width: 720px;
+    transition: all 0.3s; //a nice transition effect when hovering over
+    object-fit: cover; //centers the image in to the thumbnail dimensions
+    border-radius: 20px;
+    animation: animateThumb 0.5s; //name the animation animateThumb. Saw that the thumbs take a small amount of time to load and actually flash which creates a 'white' apearance before they are loaded. BUT, this animation will make it appear white to full opacity which looks much nicer.
+
+    :hover {
+        opacity: 0.8;
+    }
+
+    @keyframes animateThumb {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+
+`;
