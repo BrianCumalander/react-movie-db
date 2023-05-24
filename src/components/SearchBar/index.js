271d80335f1f1f73fa73d12a655b60ea978792da
BrianCumalander <koomo24@gmail.com>
2023-05-24 09:14:21 -0600

    updated .gitignore

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -12,30 +12,29 @@ const SearchBar = ({ setSearchTerm }) => {
     const initial = useRef(true) // prevents from running on initial load
 
     //add a delay for the input search to wait a moment before searching. Creates a better user experience
-    //2:53:17
     useEffect(() => {
         //useRef won't trigger a re-render, but setState will trigger a re-render. That's why here we are asking for a useRef
         if (initial.current) {
             initial.current = false;
             return;
         }
-        
+
         const timer = setTimeout(() => {
             setSearchTerm(state);
         }, 500)
         return () => clearTimeout(timer) //resets the timer
-    },[setSearchTerm, state])
+    }, [setSearchTerm, state])
 
     return (
         //input --creating an inline function. We invoke as it happens, -we want to grab the value that's in the input field. 
         <Wrapper>
             <Content>
                 <img src={searchIcon} alt='search-icon' />
-                <input 
-                type='text' 
-                placeholder="Search Movie" 
-                onChange={event => setState(event.currentTarget.value)}
-                value={state}
+                <input
+                    type='text'
+                    placeholder="Search Movie"
+                    onChange={event => setState(event.currentTarget.value)}
+                    value={state}
                 />
             </Content>
         </Wrapper>
