3a4b3354b26a00104ff9f18c4cdf30c5cbb568e0
BrianCumalander <koomo24@gmail.com>
2023-05-29 15:29:55 -0600

    Thumb- links to thumbs

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,16 +1,31 @@
 //first import React from react
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 // Styles
 //We're going to import some more things so that the thumbnails are clickable, but thats after we've setup routing 
 // and our different routes
 import { Image } from './Thumb.styles';
 
+// Hook
+//import { useHomeFetch } from '../../hooks/useHomeFetch';
+
+// Image
+import NoImage from '../../images/no_image.jpg';
+
+
+
 // setup our image prop
 const Thumb = ({ image, movieId, clickable }) => (
     //creating implicit return
     <div>
-        <Image src={image} alt='movie-thumb' />
+        {clickable ? (
+            <Link to={`/${movieId}`}>
+                <Image src={image} alt='movie-thumb' />
+            </Link>
+        ) : (
+            <Image src={image} alt='movie-thumb' />
+        )
+        }
     </div>
 );
 
