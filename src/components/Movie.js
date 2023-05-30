75e69f992f6952d658489669e2872cb26d77cf40
BrianCumalander <koomo24@gmail.com>
2023-05-30 08:26:47 -0600

    useMovieFetch repaired grabbing data

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+ 
 // Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 
@@ -7,13 +9,18 @@ import Grid from './Grid';
 import Spinner from './Spinner';
 
 // Hook
-
-
+import { useMovieFetch } from '../hooks/useMovieFetch';
 // Images
 import NoImage from '../images/no_image.jpg';
 
 
 const Movie = () => {
+    const { movieId } = useParams();
+
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+
+    console.log(movie);
+    
     return (
         <>
             <div>Movies</div>
--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -2,22 +2,48 @@ import { useState, useEffect } from 'react';
 // API
 import API from '../API';
 
-// using a perameter, movieId 
+// create the actual hook. using a perameter, movieId 
 export const useMovieFetch = movieId => {
-    //create 3 states for that component
+    //create 3 states and setters for the states. States are for that component
     const [state, setState] = useState({});
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
 
     useEffect(() => {
         //only need one useEffect because the data for all of the movie is only being loaded once, and thats on the initial load of the movie page.
-        const fetchData = async () => {
+        const fetchMovie = async () => {
             try {
-setLoading()
-            } catch (error) {
- setError(true);
-            }
-        }
-    }, [movieId]) //dependancy array. Only only changes if the movie ID changes
+                setLoading(true);
+                setError(false);
 
-}
+                //grab the data that we want -the movie from the id and credits
+                const movie = await API.fetchMovie(movieId);
+                const credits = await API.fetchCredits(movieId);
+                // Get directors only by filtering 
+                const directors = credits.crew.filter(
+                    member => member.job === 'Director'
+                );
+
+                setState({
+                    ...movie,
+                    actors: credits.cast,
+                    directors
+                })
+
+                setLoading(false);
+            } catch (error) {
+                setError(true);
+            }
+        };
+
+        fetchMovie();
+        }, [movieId]);
+
+    //     useEffect(() => {
+    //         fetchMovie();
+    //     }, [movieId, fetchMovie]);
+    // })
+
+
+    return { state, loading, error };
+};
