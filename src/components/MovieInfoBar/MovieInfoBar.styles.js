522caf1b18861f2c4237b5de86ff5d8813dddff1
BrianCumalander <koomo24@gmail.com>
2023-06-03 13:13:26 -0600

    styled MovieInfoBar

--- src/components/MovieInfoBar/MovieInfoBar.styles.js
+++ src/components/MovieInfoBar/MovieInfoBar.styles.js
@@ -1,11 +1,42 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    min-height: 100px;
+    background: var(--darkGrey); /* background color */
+    padding: 0 20px; /* gives it a nice padding all the way around */
+`;
 
+export const Content = styled.div`
+    display: flex;
+    max-width: var(--maxWidth); /* sets the max width, but below will display it */
+    width: 100%; /* controls how wide the content spans */
+    margin: 0 auto; /* sets the margin of the content */
 
+    .column {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: var(--medGrey);
+        border-radius: 20px;
+        margin: 0 20px;
+        flex: 1;
 
-export const Content = styled.div``;
+        :first-child {
+            margin-left: 0;
+        }
 
+        :last-child {
+            margin-right: 0;
+        }
+    }
 
+    @media screen and (max-width: 768px) {
+        display: block;
 
-export const Text = styled.div``;
+        .column {
+            margin: 20px 0;
+        }
+    }
+`;
